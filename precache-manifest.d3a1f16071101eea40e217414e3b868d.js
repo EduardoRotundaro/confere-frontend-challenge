d4752df51df45fc43b0115c25600f97a90c20617
Eduardo Rotundaro <eduardorotundaro@gmail.com>
2020-03-16 22:11:54 -0300

    Updates

---
+++ precache-manifest.d3a1f16071101eea40e217414e3b868d.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b6a1af0353f68b10f70e703caae1685",
+    "url": "/confere-frontend-challenge/index.html"
+  },
+  {
+    "revision": "e1ed2a3f1bd1267f9a00",
+    "url": "/confere-frontend-challenge/static/css/main.07f08c0a.chunk.css"
+  },
+  {
+    "revision": "d4facd60a7adef1cb2f6",
+    "url": "/confere-frontend-challenge/static/js/2.bb168b58.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/confere-frontend-challenge/static/js/2.bb168b58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ed2a3f1bd1267f9a00",
+    "url": "/confere-frontend-challenge/static/js/main.b0527f45.chunk.js"
+  },
+  {
+    "revision": "3709894e756bdbb1b652",
+    "url": "/confere-frontend-challenge/static/js/runtime-main.0c722cb0.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/confere-frontend-challenge/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/confere-frontend-challenge/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/confere-frontend-challenge/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/confere-frontend-challenge/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/confere-frontend-challenge/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "e4982e4a3fa90be5ddb107937f98e7d7",
+    "url": "/confere-frontend-challenge/static/media/productPhotoFallback.e4982e4a.png"
+  }
+]);
