1e7215edf5045715f1a6619d4ec698bb0de16e7f
Eduardo Rotundaro <eduardorotundaro@gmail.com>
2020-03-16 21:58:08 -0300

    :sparkles: adding 404 page

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 export default function Home() {
     return (
-        <></>
+        <main className="home">
+            <section>
+                <div className="container">
+                    <div className="row mt-4">
+                        <div className="col-12 mt-4">
+                            <div className="jumbotron mt-4">
+                                <h1 className="display-4">404</h1>
+                                <p className="lead">Página não encontrada.</p>
+                                <hr className="my-4"/>
+                                <Link to="/home" className="btn btn-primary btn-lg btn-block" role="button">Ir para a página inicial</Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 }
